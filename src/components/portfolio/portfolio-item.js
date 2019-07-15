e1c106e63461c5d1f4e68d5710194ad646a43842
stewylad <stew01art@gmail.com>
2019-07-15 08:26:04 -0600

    minor image changes

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -8,21 +8,25 @@ export default function(props) {
         //-description: description
         //-id: id
 
-    const { id, description, thumb_image_url, logo } = props.item;
+    const { id, description, thumb_image_url, logo_url } = props.item;
     return (
-        <div className="portfolio-item-wrapper">
-            <div
-                className="portfolio-img-background"
-                style={{
-                    backgroundImage: "url(" + thumb_image_url + ")"
-                }}
-            />
+        <Link to={'/portfolio/${id}'}>
+            <div className="portfolio-item-wrapper">
+                <div
+                    className="portfolio-img-background"
+                    style={{
+                        backgroundImage: "url(" + thumb_image_url + ")"
+                    }}
+                />
 
-
-            <img src={thumb_image_url} />
-            <img src={logo} />
-            <div>{description}</div>
-            <Link to={`/portfolio/${id}`}>{props.title}</Link>
-        </div>
+                <div className="img-text-wrapper">
+                    <div className="logo-wrapper">
+                        <img src={logo_url} />
+                    </div>
+                    
+                    <div className="subtitle">{description}</div>
+                </div>
+            </div>
+        </Link>
     );
 }
